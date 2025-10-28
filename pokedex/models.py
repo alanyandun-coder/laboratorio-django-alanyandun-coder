68aefcaecedc7a88ee752177b2b03bd3adecc784
alanyandun-codern <alan.yandun@uisek.edu.ec>
2025-10-27 22:37:14 -0500

    Entrega final del laboratorio Django

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    type = models.CharField(max_length=30, null=False)
+    weight = models.DecimalField(decimal_places=4, max_digits=6)
+    height = models.DecimalField(decimal_places=4, max_digits=6)
+
+    def __str__(self):
+        return self.name   
